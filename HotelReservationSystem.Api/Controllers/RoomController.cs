90c91198297dc18ad54c8a02fb20bb664701ceb7
kamel <kamelsamy3@gmail.com>
2025-06-27 03:40:08 +0300

    create room controller and add the main actions

--- HotelReservationSystem.Api/Controllers/RoomController.cs
+++ HotelReservationSystem.Api/Controllers/RoomController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using HotelReservation.DataAccess.Models;
+using HotelReservationSystem.BL.DTOs.Hotel;
+using HotelReservationSystem.BL.DTOs.Room;
+using HotelReservationSystem.Core.Interfaces;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HotelReservationSystem.Api.Controllers
@@ -7,6 +11,83 @@ namespace HotelReservationSystem.Api.Controllers
     [ApiController]
     public class RoomController : ControllerBase
     {
+        private readonly IBaseRepository<Room> _roomRepository;
+        public RoomController(IBaseRepository<Room> roomRepository)
+        {
+            _roomRepository = roomRepository;
+        }
 
+        [HttpGet]
+        public async Task<IActionResult> GetAllRooms()
+        {
+            var rooms = await _roomRepository.GetAllAsync();
+            if (rooms == null || rooms.Count == 0)
+            {
+                return NotFound("No rooms found.");
+            }
+            return Ok(rooms);
+        }
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetRoomById(int id)
+        {
+            var room = await _roomRepository.GetByIDAsync(id);
+            if (room == null)
+            {
+                return NotFound($"Room with ID {id} not found.");
+            }
+            return Ok(room);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRoom(AddRoomDto roomDto)
+        {
+            Room room = new Room
+            {
+                roomNumber = roomDto.roomNumber,
+                roomType = roomDto.roomType,
+                Description = roomDto.Description,
+                Beds = roomDto.Beds,
+                PricePerNight = roomDto.PricePerNight,
+                IsAvailable = roomDto.IsAvailable
+            };
+            if (room == null)
+            {
+                return BadRequest("Room data is null.");
+            }
+            await _roomRepository.AddAsync(room);
+            await _roomRepository.SaveAsync();
+            return CreatedAtAction(nameof(GetRoomById), new { id = room.Id }, room);
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> UpdateRoom(int id, AddRoomDto roomDto)
+        {
+            var existingRoom = await _roomRepository.GetByIDAsync(id);
+            if (existingRoom == null)
+            {
+                return NotFound($"Room with ID {id} not found.");
+            }
+            existingRoom.roomNumber = roomDto.roomNumber;
+            existingRoom.roomType = roomDto.roomType;
+            existingRoom.Description = roomDto.Description;
+            existingRoom.Beds = roomDto.Beds;
+            existingRoom.PricePerNight = roomDto.PricePerNight;
+            existingRoom.IsAvailable = roomDto.IsAvailable;
+            await _roomRepository.UpdateAsync(existingRoom);
+            await _roomRepository.SaveAsync();
+            return Ok($"The Room {existingRoom.roomNumber} Updated Successfully");
+        }
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeleteRoom(int id)
+        {
+            var room = await _roomRepository.GetByIDAsync(id);
+            if (room == null)
+            {
+                return NotFound($"Room with ID {id} not found.");
+            }
+            await _roomRepository.DeleteAsync(room);
+            await _roomRepository.SaveAsync();
+            return Ok($"The room {room.roomNumber} Deleted Successfully");
+        }
     }
 }
---
+++ HotelReservationSystem.BL/DTOs/Room/AddRoomDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationSystem.BL.DTOs.Room
+{
+    public class AddRoomDto
+    {
+        public int Id { get; set; }
+        public string roomNumber { get; set; }
+        public string roomType { get; set; }
+        public string? Description { get; set; }
+        public int Beds { get; set; }
+        public decimal PricePerNight { get; set; }
+        public bool IsAvailable { get; set; }
+    }
+}
