aae90fa8034498f1ecfe8c28e86498047be4dd1b
kamel <kamelsamy3@gmail.com>
2025-06-27 04:12:45 +0300

    create payment controller and the main actions

---
+++ HotelReservationSystem.Api/Controllers/PaymentController.cs
@@ -0,0 +1,89 @@
+﻿using HotelReservation.DataAccess.Models;
+using HotelReservationSystem.BL.DTOs.Hotel;
+using HotelReservationSystem.BL.DTOs.Payment;
+using HotelReservationSystem.Core.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelReservationSystem.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        private readonly IBaseRepository<Payment> _paymentRepository;
+        public PaymentController(IBaseRepository<Payment> paymentRepository)
+        {
+            _paymentRepository = paymentRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllPayment()
+        {
+            var payments = await _paymentRepository.GetAllAsync();
+            if (payments == null || payments.Count == 0)
+            {
+                return NotFound("No Payments found.");
+            }
+            return Ok(payments);
+        }
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetPaymentById(int id)
+        {
+            var payment = await _paymentRepository.GetByIDAsync(id);
+            if (payment == null)
+            {
+                return NotFound($"Hotel with ID {id} not found.");
+            }
+            return Ok(payment);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreatePayment(addPaymentDto paymentDto)
+        {
+            Payment payment = new Payment
+            {
+                Amount = paymentDto.Amount,
+                PaymentMethod = paymentDto.PaymentMethod,
+                PaymentDate = DateTime.Now,
+                Status = paymentDto.Status,
+            };
+            if (payment == null)
+            {
+                return BadRequest("Hotel data is null.");
+            }
+            await _paymentRepository.AddAsync(payment);
+            await _paymentRepository.SaveAsync();
+            return CreatedAtAction(nameof(GetPaymentById), new { id = payment.Id }, payment);
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> UpdatePayment(int id, addPaymentDto paymentDto)
+        {
+            var existingPayment = await _paymentRepository.GetByIDAsync(id);
+            if (existingPayment == null)
+            {
+                return NotFound($"Payment with ID {id} not found.");
+            }
+            existingPayment.Amount = paymentDto.Amount;
+            existingPayment.PaymentMethod = paymentDto.PaymentMethod;
+            existingPayment.PaymentDate = DateTime.Now;
+            existingPayment.Status = paymentDto.Status;
+            await _paymentRepository.UpdateAsync(existingPayment);
+            await _paymentRepository.SaveAsync();
+            return Ok($"The Payment with ID {existingPayment.Id} Updated Successfully");
+        }
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeletePayment(int id)
+        {
+            var payment = await _paymentRepository.GetByIDAsync(id);
+            if (payment == null)
+            {
+                return NotFound($"Payment with ID {id} not found.");
+            }
+            await _paymentRepository.DeleteAsync(payment);
+            await _paymentRepository.SaveAsync();
+            return Ok($"The Payment with ID {payment.Id} Deleted Successfully");
+        }
+    }
+}
---
+++ HotelReservationSystem.BL/DTOs/Payment/addPaymentDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationSystem.BL.DTOs.Payment
+{
+    public class addPaymentDto
+    {
+        public decimal Amount { get; set; }
+        public DateTime PaymentDate { get; set; }
+        public string PaymentMethod { get; set; }
+        public string Status { get; set; }
+    }
+}
